e57a5987c0ab46a5040e1f0138618ddcc7aea9a9
JAYA MEHTA <82704184+jayamehta1@users.noreply.github.com>
2025-10-01 19:55:40 +0530

    Add files via upload

---
+++ waterBottels.py
@@ -0,0 +1,20 @@
+"""Water Bottles
+There are numBottles water bottles that are initially full of water. You can exchange numExchange empty water bottles from the market with one full water bottle.
+The operation of drinking a full water bottle turns it into an empty bottle.
+Given the two integers numBottles and numExchange, return the maximum number of water bottles you can drink.
+"""
+
+
+def numWaterBottles(numBottles, numExchange):
+    drink = numBottles
+    new = 0
+    while numBottles >= numExchange:
+        rem = numBottles % numExchange
+        new = numBottles // numExchange
+        numBottles = new + rem
+        drink += new
+    return drink
+
+
+print(numWaterBottles(9, 3))  # Output: 13
+print(numWaterBottles(15, 4))  # Output: 19
