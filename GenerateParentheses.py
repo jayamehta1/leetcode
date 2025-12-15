619e7ce5e9638e5a3bba5b3ef6f50bb2408c2dd4
JAYA MEHTA <82704184+jayamehta1@users.noreply.github.com>
2025-12-15 20:46:36 +0530

    Add files via upload

---
+++ GenerateParentheses.py
@@ -0,0 +1,28 @@
+'''Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+Example 1:
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+Example 2:
+Input: n = 1
+Output: ["()"]'''
+
+
+def generateParenthesis(n):
+    def backtrack(s, left, right):
+        if len(s) == 2 * n:
+            result.append(s)
+            return
+        if left < n:
+            backtrack(s + '(', left + 1, right)
+        if right < left:
+            backtrack(s + ')', left, right + 1)
+
+    result = []
+    backtrack('', 0, 0)
+    return result
+
+
+# Output: ["((()))","(()())","(())()","()(())","()()()"]
+print(generateParenthesis(3))
+print(generateParenthesis(1))  # Output: ["()"]
+print(generateParenthesis(2))  # Output: ["(())","()()"]
