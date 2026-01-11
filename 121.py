ea4b1ad51ee0495677b14b5bdc90f6c02c457076
JAYA MEHTA <82704184+jayamehta1@users.noreply.github.com>
2026-01-11 20:17:51 +0530

    Add files via upload

---
+++ 121.py
@@ -0,0 +1,16 @@
+def maxProfit(prices):
+
+    min_price = float('inf')
+    max_profit = 0
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        profit = price - min_price
+        if profit > max_profit:
+            max_profit = profit
+
+    return max_profit
+
+
+print(maxProfit([7, 1, 5, 3, 6, 4]))  # Output: 5
+print(maxProfit([7, 6, 4, 3, 1]))  # Output: 0
