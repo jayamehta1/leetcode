9d21d6c2800c68d127fafc66f4c5376ff3edb12b
JAYA MEHTA <82704184+jayamehta1@users.noreply.github.com>
2026-01-13 20:54:41 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,25 @@
+'''Q1. Set Mismatch
+You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+You are given an integer array nums representing the data status of this set after the error.
+Find the number that occurs twice and the number that is missing and return them in the form of an array.
+
+'''
+
+
+def errorNums(nums):
+    n = len(nums)
+    nums.sort()
+    duplicate = -1
+    missing = -1
+    for i in range(1, n):
+        if nums[i] == nums[i-1]:
+            duplicate = nums[i]
+        elif nums[i] > nums[i-1] + 1:
+            missing = nums[i-1] + 1
+    if nums[n-1] != n:
+        missing = n
+    return [duplicate, missing]  # Output: 2
+
+
+print(errorNums([1, 2, 2, 4]))  # Output: [2, 3]
+print(errorNums([1, 1]))  # Output: [1, 2]
