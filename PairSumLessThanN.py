a2ee5c151ab9d1f3bf256921b555dfd5becb9c35
JAYA MEHTA <82704184+jayamehta1@users.noreply.github.com>
2026-02-11 22:19:29 +0530

    Add files via upload

---
+++ PairSumLessThanN.py
@@ -0,0 +1,9 @@
+def pairSumLessThanN(nums, target):
+    count = 0
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i] + nums[j] < target:
+                count += 1
+    return count
+
+print(pairSumLessThanN([1,2,3,4,5], 10))    #10
