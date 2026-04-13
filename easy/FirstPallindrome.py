638c6253abb616fedfae7e6796207c9c0a899360
JAYA MEHTA <82704184+jayamehta1@users.noreply.github.com>
2026-04-13 20:39:15 +0530

    Add files via upload

---
+++ easy/FirstPallindrome.py
@@ -0,0 +1,30 @@
+'''
+
+2108. Find First Palindromic String in the Array
+s
+Hint
+Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".
+
+A string is palindromic if it reads the same forward and backward.
+
+ 
+
+Example 1:
+
+Input: words = ["abc","car","ada","racecar","cool"]
+Output: "ada"
+Explanation: The first string that is palindromic is "ada".
+Note that "racecar" is also palindromic, but it is not the first.'''
+
+class Solution:
+    def firstPalindrome(self, words):
+        for i in range(len(words)):
+            if words[i][::-1] == words[i]:
+                return words[i]
+        return ""
+
+sol = Solution()
+
+words = ["abc", "car", "ada", "racecar", "cool"]
+res = sol.firstPalindrome(words)
+print(res)
