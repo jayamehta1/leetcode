bb0d40aa3ae69ab9cb1b4d9a9cee64fbe77f2f09
JAYA MEHTA <82704184+jayamehta1@users.noreply.github.com>
2026-09-09 23:00:22 +0530

    Create 3870. Count Commas in Range.py

---
+++ easy/3870. Count Commas in Range.py
@@ -0,0 +1,28 @@
+""" 3870. Count Commas in Range
+You are given an integer n.
+Return the total number of commas used when writing all integers from [1, n] (inclusive) in standard number formatting.
+In standard formatting:
+A comma is inserted after every three digits from the right.
+Numbers with fewer than 4 digits contain no commas.
+ 
+
+Example 1:
+
+Input: n = 1002
+
+Output: 3
+
+Explanation:
+
+The numbers "1,000", "1,001", and "1,002" each contain one comma, giving a total of 3."""
+
+
+def countCommas(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 1000:
+            return 0
+        else :
+            return (n - 1000) + 1    
